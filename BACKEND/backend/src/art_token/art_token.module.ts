c49354053d7e73ef36b2f47dd80e82473f503d63
juangutierrezjp <121174752+juangutierrezjp@users.noreply.github.com>
2025-06-30 17:13:16 -0300

    Updated trace

--- BACKEND/backend/src/art_token/art_token.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ArtTokenController } from './art_token.controller';
-import { ArtTokenService } from './art_token.service';
-
-@Module({
-  controllers: [ArtTokenController],
-  providers: [ArtTokenService],
-})
-export class ArtTokenModule { }
